fe3d1b3bd87c073c65a20fde7f3a3510ba105e2f
JohnQuimson <john.quimson777@gmail.com>
2023-11-25 01:49:31 +0100

    BONUS: aggiunta img

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ createApp({
   setup() {
     return {
       titolo: `Primo approccio con VueJS`,
+      foto: `https://images.unsplash.com/photo-1554744512-d6c603f27c54?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
     };
   },
 }).mount('#app');
